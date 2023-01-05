5ddfb1d0688dfc677ddd5f7c725952c006b5e406
Julie Nijs <107923146+nysju@users.noreply.github.com>
2023-01-05 11:25:44 +0100

    Update gzip_files.py

--- Experiment 1 EDGe corpus/code/gzip_files.py
+++ Experiment 1 EDGe corpus/code/gzip_files.py
@@ -1,12 +1,15 @@
+# Python code for zipping all files in a directory and writing them to a new directory, using gzip
+
 import gzip
 import os
 import glob
 
 
 if __name__ == '__main__':
-    os.chdir(r'C:/Users/u0149275/OneDrive - KU Leuven/Demography/Datasets/EDGe/EDGe_Syntactic_Deletion')
+    os.chdir(r'C:/Users/EDGe') # insert directory that contains the files
     my_files = glob.glob('*.txt')
-    path = r'C:/Users/u0149275/OneDrive - KU Leuven/Demography/Datasets/EDGe/EDGe_Syntactic_Deletion_Zipped/'
+    path = r'C:/Users/EDGe/Zipped/' # insert directory to send the zipped files to
+    # iterate over files and gzip them
     for my_file in my_files:
         with open(my_file, 'rb') as f_in, gzip.open(path + my_file + '.gz', 'wb') as f_out:
             f_out.writelines(f_in)
