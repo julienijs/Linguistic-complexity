f5b334bb0c57e536ab9c562018a5ac5b82e3b35b
Julie Nijs <107923146+julienijs@users.noreply.github.com>
2024-05-19 15:58:24 +0200

    Update complexity_analysis.R

--- Experiment 1 EDGe corpus/code/complexity_analysis.R
+++ Experiment 1 EDGe corpus/code/complexity_analysis.R
@@ -1,16 +1,16 @@
 # This is the r script for analyzing the morphological and syntactic complexity datasets
 
-#### load libraries ####
-
 library(readxl)
 library(ggplot2)
 library(matrixStats)
 library(effects)
+library(ppcor)
+library(dplyr)
 
 #### morphology ####
 
 # read data
-morph_data <- read_xlsx("morph_zipped_all.xlsx", col_names = TRUE)
+morph_data <- read_xlsx("EDGe_Morph_Zipped.xlsx", col_names = TRUE)
 zipped <- read_xlsx("EDGe_Zipped_Sizes.xlsx", col_names = TRUE)
 
 # merge data
@@ -23,28 +23,22 @@ morph_total[,8:1007] <- morph_total[,8:1007]/morph_total[,5]
 # get means for each row = get mean complexity ratio
 morph_means <- rowMeans(morph_total[,8:1007])
 # add mean complexity ratios to data frame
-morph_total$morph_means <- morph_means
+morph_total$morph_means <- as.numeric(morph_means)
 # get standard deviations
 morph_std = rowSds(as.matrix(morph_total[,8:1007]))
 
+morph_total <- morph_total %>%
+  mutate(language_bin = ifelse(language == "English", "English", "Continental"))
 
 # linear model
 morph_model <- lm(morph_means ~ year + language, data=morph_total)
-morph_model <- lm(morph_means ~ year*language, data=morph_total)
 summary(morph_model)
-plot(allEffects(morph_model))
-
-# make plots
-
-mp <- ggplot(morph_total, 
-             aes(x = year, y = morph_means, color = language, shape = text))+
-  ggtitle("Morphological complexity ratio over time") +
-  xlab("Year")+
-  ylab("Mean morphological complexity ratio")+
-  guides(color=guide_legend(title="Language"), 
-         shape=guide_legend(title="Text"))+
-  geom_point()
-mp
+morph_Dutch <- lm(morph_means ~ year, data = subset(morph_total, language == 'Dutch'))
+summary(morph_Dutch)
+morph_German <- lm(morph_means ~ year, data = subset(morph_total, language == 'German'))
+summary(morph_German)
+morph_English <- lm(morph_means ~ year, data = subset(morph_total, language == 'English'))
+summary(morph_English)
 
 ml <- ggplot(morph_total, 
              aes(x = year, y = morph_means, color = language))+
@@ -52,23 +46,51 @@ ml <- ggplot(morph_total,
   xlab("Year")+
   ylab("Mean morphological complexity ratio")+
   guides(color=guide_legend(title="Language"))+
-  geom_point()+
-  geom_smooth()
+  geom_point()
 ml
 
-mbp <- ggplot(morph_total, 
-             aes(x = language, y = morph_means, fill = text))+
-  ggtitle("Morphological complexity ratio aggregated by language") +
-  xlab("Language")+
-  ylab("Mean morphological complexity ratio")+
-  guides(fill=guide_legend(title="Text"))+
-  geom_boxplot()
-mbp
-
-#### syntaxis ####
+#### morphology suffix ####
 
 # read data
-synt_data <- read_xlsx("synt_zipped_all.xlsx", col_names = TRUE)
+morph_suffix_data <- read_xlsx("EDGe_Morph_Suffix_Zipped.xlsx", col_names = TRUE)
+
+# merge data
+morph_suffix_total <- merge(zipped, morph_suffix_data, by="...1")
+
+# make all numbers negative
+morph_suffix_total[,8:1007] <- morph_suffix_total[,8:1007]*(-1)
+# divide by full zipped size to get complexity ratio
+morph_suffix_total[,8:1007] <- morph_suffix_total[,8:1007]/morph_suffix_total[,5]
+# get means for each row = get mean complexity ratio
+morph_suffix_means <- rowMeans(morph_suffix_total[,8:1007])
+# add mean complexity ratios to data frame
+morph_suffix_total$morph_suffix_means <- as.numeric(morph_suffix_means)
+# get standard deviations
+morph_suffix_std = rowSds(as.matrix(morph_suffix_total[,8:1007]))
+
+# linear model
+morph_suffix_model <- lm(morph_suffix_means ~ year + language, data=morph_suffix_total)
+summary(morph_suffix_model)
+morph_suffix_Dutch <- lm(morph_suffix_means ~ year, data = subset(morph_suffix_total, language == 'Dutch'))
+summary(morph_suffix_Dutch)
+morph_suffix_German <- lm(morph_suffix_means ~ year, data = subset(morph_suffix_total, language == 'German'))
+summary(morph_suffix_German)
+morph_suffix_English <- lm(morph_suffix_means ~ year, data = subset(morph_suffix_total, language == 'English'))
+summary(morph_suffix_English)
+
+msl <- ggplot(morph_suffix_total, 
+             aes(x = year, y = morph_suffix_means, color = language))+
+  ggtitle("Morphological complexity ratio over time (suffix)") +
+  xlab("Year")+
+  ylab("Mean morphological complexity ratio")+
+  guides(color=guide_legend(title="Language"))+
+  geom_point()
+msl
+
+#### syntax ####
+
+# read data
+synt_data <- read_xlsx("EDGe_Synt_Zipped.xlsx", col_names = TRUE)
 
 # merge data
 synt_total <- merge(zipped, synt_data, by="...1")
@@ -82,58 +104,55 @@ synt_total$synt_means <- synt_means
 # get standard deviations
 synt_std = rowSds(as.matrix(synt_total[,8:1007]))
 
-
 # linear model
 synt_model <- lm(synt_means ~ year+language, data=synt_total)
-synt_model <- lm(synt_means ~ year*language, data=synt_total)
 summary(synt_model)
-plot(allEffects(synt_model))
-
-# make plots
-
-sp <- ggplot(synt_total, 
-             aes(x = year, y = synt_means, color = language, shape = text))+
-  ggtitle("Syntactic complexity ratio over time") +
-  xlab("Year")+
-  ylab("Mean syntactic complexity ratio")+
-  guides(color=guide_legend(title="Language"), 
-         shape=guide_legend(title="Text"))+
-  geom_point()
-sp
+synt_Dutch <- lm(synt_means ~ year, data = subset(synt_total, language == 'Dutch'))
+summary(synt_Dutch)
+synt_English <- lm(synt_means ~ year, data = subset(synt_total, language == 'English'))
+summary(synt_English)
+synt_German <- lm(synt_means ~ year, data = subset(synt_total, language == 'German'))
+summary(synt_German)
 
 sl <- ggplot(synt_total, 
              aes(x = year, y = synt_means, color = language))+
-  ggtitle("Syntactic complexity ratio over time") +
+  ggtitle("Syntactic complexity over time") +
   xlab("Year")+
-  ylab("Mean syntactic complexity ratio")+
+  ylab("Mean syntactic complexity")+
   guides(color=guide_legend(title="Language"))+
-  geom_point()+
-  geom_smooth()
+  geom_point()
 sl
 
-sbp <- ggplot(synt_total, 
-              aes(x = language, y = synt_means, fill = text))+
-  ggtitle("Syntactic complexity ratio aggregated by language") +
-  xlab("Language")+
-  ylab("Mean syntactic complexity ratio")+
-  guides(fill=guide_legend(title="Text"))+
-  geom_boxplot()
-sbp
+#### morphology vs syntax ####
 
-#### morphology vs syntaxis ####
+morph_and_synt <- merge(synt_total, morph_total, by = "filename", all = TRUE)
 
-morph_and_synt <- data.frame(morph_total$morph_means, 
-                             synt_total$synt_means, 
-                             synt_total$language,
-                             synt_total$text,
-                             synt_total$year)
-
+# linear model
 morph_and_synt_model <- lm(synt_means ~ morph_means, data=morph_and_synt)
 summary(morph_and_synt_model)
 
 scp <- ggplot(morph_and_synt,
-              aes(x = synt_means, y = morph_means, color = synt_total.language))+
-  ggtitle("Syntactic vs morphological complexity ratio") +
+              aes(x = synt_means, y = morph_means, color = language.x))+
+  ggtitle("Trade-off between morphological complexity and syntactic complexity") +
+  xlab("Mean syntactic complexity ratio")+
+  ylab("Mean morphological complexity ratio")+
+  guides(color=guide_legend(title="Language"))+
+  geom_point()
+scp
+
+#### morphology suffix vs syntax ####
+
+morph_and_synt <- merge(morph_and_synt, morph_suffix_total, by = "filename", all = TRUE)
+
+
+# linear model
+morph_suffix_and_synt_model <- lm(synt_means ~ morph_suffix_means, data=morph_and_synt)
+summary(morph_suffix_and_synt_model)
+
+scp <- ggplot(morph_and_synt,
+              aes(x = synt_means, y = morph_suffix_means, 
+                  color = language.x))+
+  ggtitle("Morphological complexity ratio (more restrictive) vs syntactic complexity ratio") +
   xlab("Mean syntactic complexity ratio")+
   ylab("Mean morphological complexity ratio")+
   guides(color=guide_legend(title="Language"))+
